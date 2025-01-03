1580d836467b59260e4c99f604ea578d851dcac4
edwardlorilla2056 <edwardlorilla2056@gmail.com>
2025-01-03 18:49:46 +0800

    Update blog_generator.py

--- blog_generator.py
+++ blog_generator.py
@@ -1098,10 +1098,10 @@ if __name__ == "__main__":
     
     # List of recipients
     recipients = [
-        "edwardlorilla2048.edwardlancelorilla@blogger.com",
-        "edwardlorilla2061.edwardlancelorilla@blogger.com",
-        "edwardlorilla2064.edwardlancelorilla@blogger.com",
-        "edwardlorilla2064.edwardlancelorilla1@blogger.com",
+        "edwardlorilla2081.edwardlancelorilla@blogger.com",
+        "edwardlorilla2082.edwardlancelorilla@blogger.com",
+        "edwardlorilla2088.edwardlancelorilla@blogger.com",
+        "edwardlorilla2091.edwardlancelorilla1@blogger.com",
     ]
     for recipient in recipients:
         topic, word_count, audience = generate_random_inputs()
